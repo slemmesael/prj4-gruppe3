a63824bcc690346d4e4b9d397d380c6480b11ce5
Rikke <201704464@post.au.dk>
2019-05-16 14:25:48 +0200

    socket og server der virker

---
+++ server/version 0.5/testSocket/build/host/release/server.d
@@ -0,0 +1,14 @@
+build/host/release/server.o: src/Web/server.cpp src/Web/server.hpp \
+ lib/osApi/inc/osapi/Message.hpp lib/osApi/inc/osapi/MsgQueue.hpp \
+ lib/osApi/inc/osapi/Mutex.hpp lib/osApi/inc/osapi/MutexError.hpp \
+ lib/osApi/inc/osapi/Exceptions.hpp lib/osApi/inc/osapi/linux/Mutex.hpp \
+ lib/osApi/inc/osapi/Conditional.hpp lib/osApi/inc/osapi/Utility.hpp \
+ lib/osApi/inc/osapi/linux/Conditional.hpp \
+ lib/osApi/inc/osapi/ConditionalError.hpp \
+ lib/osApi/inc/osapi/ThreadFunctor.hpp \
+ lib/osApi/inc/osapi/linux/ThreadFunctor.hpp \
+ lib/osApi/inc/osapi/Completion.hpp src/Web/socket.hpp lib/uWS/uWS/uWS.h \
+ lib/uWS/uWS/Hub.h lib/uWS/uWS/Group.h lib/uWS/uWS/WebSocket.h \
+ lib/uWS/uWS/WebSocketProtocol.h lib/uWS/uWS/Networking.h \
+ lib/uWS/uWS/Backend.h lib/uWS/uWS/Epoll.h lib/uWS/uWS/Socket.h \
+ lib/uWS/uWS/HTTPSocket.h lib/uWS/uWS/Extensions.h lib/uWS/uWS/Node.h
